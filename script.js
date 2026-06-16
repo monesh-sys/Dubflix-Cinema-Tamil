2fdd313115e9318b09721a72303164885aef3e1a
moneshtech <moneshsys@gmail.com>
2026-06-16 19:55:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const movies = [
 {
     title: "Summer House",
     category: "romantic",
-    image: "images/Kubaraa.jpg",
+    image: "images/Kubaraa.png",
     link: "https://drive.google.com/file/d/1U098Pq4C2jr3ZbXe122BSjBR7JQSayHk/view?usp=sharing"
 }
 ];
